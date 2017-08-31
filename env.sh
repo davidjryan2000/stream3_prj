144ca46dd9a8e01e10e8938da7deddd8b3034b2f
David Ryan <davidjryan2000@gmail.com>
2017-08-31 15:22:08 +0100

    add Procfile

--- env.sh
+++ env.sh
@@ -5,4 +5,8 @@ export DEBUG=True
 
 export STRIPE_PUBLISHABLE_KEY='pk_test_HpjDh6Lu6A27SgKH90ANgPuY'
 export STRIPE_SECRET_KEY='sk_test_9gmvzCpJrxorHGFMrqveMcYn'
+export DATABASE_URL="postgres://ljtlursdqntalk:55f80030751603ba8a6c08813315676c190627412859b5ec16f10f14dfac6b63@ec2-54-75-227-173.eu-west-1.compute.amazonaws.com:5432/d6eun1cbhqm9eh"
+export AWS_STORAGE_BUCKET_NAME=stream3-prj
+export AWS_ACCESS_KEY_ID=AKIAJORL5RQQD6IBRJHA
+export AWS_SECRET_ACCESS_KEY=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
 echo Done
--- stream3_prj/settings.py
+++ stream3_prj/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG',False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stream3.herokuapp.com', 'localhost']
+INTERNAL_IPS = []
 
 
 # Application definition
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
     #'rest_framework'
     'disqus',
     'storages'
+    'psycopg2'
 ]
 
 MIDDLEWARE = [
@@ -91,13 +93,17 @@ WSGI_APPLICATION = 'stream3_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
+
+
+DATABASE_URL =  os.environ.get('DATABASE_URL')
+DATABASES = {'default': dj_database_url.parse(DATABASE_URL) }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -145,7 +151,31 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/
 
 DISQUS_WEBSITE_SHORTNAME =  os.environ.get("DISQUS_SHORTNAME")
 SITE_ID = 1
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+   'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+   'Cache-Control': 'max-age=94608000',
+}
+
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
